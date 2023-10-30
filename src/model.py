fea8f7a547fb94c9d9fcd8475e932c12c0090960
Valentin Belyaev <chuvalik.work@gmail.com>
2023-10-30 23:46:51 +0300

    Add Model interface

---
+++ src/model.py
@@ -0,0 +1,25 @@
+from abc import ABC
+
+import numpy as np
+
+
+class Model(ABC):
+    """
+    Interface to define machine learning models.
+    """
+
+    def fit(self, x: np.ndarray, y: np.ndarray):
+        """
+        Train model based on input data.
+        :param x: Training data.
+        :param y: Target feature.
+        :return:
+        """
+        pass
+
+    def predict(self, x: np.ndarray) -> np.ndarray:
+        """
+        Predict scores for input data.
+        :param x: Test data.
+        :return: Test scores.
+        """
