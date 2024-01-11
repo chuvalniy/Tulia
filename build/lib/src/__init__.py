06e66e8e7baf1e14902b61f72bbb0dad2424e28b
Valentin Belyaev <chuvalik.work@gmail.com>
2024-01-11 15:37:28 +0300

    Add files to .gitignore

--- build/lib/src/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import *
-from .ensemble import *
-from .gaussian import *
-from .neighbors import *
-from .tree import *
--- build/lib/src/linear/__init__.py
+++
@@ -1,5 +0,0 @@
-from .lasso import *
-from .linreg import *
-from .logistic import *
-from .ridge import *
-from .linear import *
